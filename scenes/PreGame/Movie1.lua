d11507b8f0a85dbb49c6ad39f793cbcc52289c97
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-07-23 20:42:06 +0100

    rewritten to work with scenes

---
+++ scenes/PreGame/Movie1.lua
@@ -0,0 +1,37 @@
+local movie1 = {}
+local pbu = require(game.ReplicatedStorage.patchbaseurl)
+local inputs = require(script.Parent.Parent.Parent.Engines.inputEngine)
+local scenes = require(script.Parent.Parent.Parent.Engines.sceneEngine)
+local rendering = require(script.Parent.Parent.Parent.Engines.renderingEngine)
+local ctrls = require(game.ReplicatedStorage.inputs)
+movie1.run = function()
+	local newsound = Instance.new("Sound")
+	newsound.Volume = 1
+
+	newsound.SoundId = pbu.."/music/1.mp3"
+	newsound.Parent = game.Players.LocalPlayer.PlayerGui
+	newsound:Play()
+	local movie = coroutine.create(function()
+		newsound.Ended:Wait()
+		newsound:Destroy()
+		scenes.openscene("PreGame.Movie2")
+	end)
+	coroutine.resume(movie)
+	local function skip()
+		newsound:Destroy()
+		coroutine.close(movie)
+		inputs.wipeAllFuncs()
+		rendering.clearGraphics()
+		scenes.openscene("PreGame.Titlescreen")
+	end
+	inputs.assignKeyToFunc(ctrls.scheme1.interact, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.cancel, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.menu, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.extra, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.up, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.left, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.down, "skipmovie", skip, true)
+	inputs.assignKeyToFunc(ctrls.scheme1.right, "skipmovie", skip, true)
+end
+
+return movie1
