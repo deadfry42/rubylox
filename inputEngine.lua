fa850b0556bf73079e152e6e832f54590737d028
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-07-23 11:56:54 +0100

    added an inputEngine (inputEngine.lua)

---
+++ inputEngine.lua
@@ -0,0 +1,47 @@
+local module = {}
+local maps = {}
+
+local uis = game:GetService("UserInputService")
+
+module.assignKeyToFunc = function(key, id, func, oneTime)
+	if maps[tostring(key)] == nil then
+		maps[tostring(key)] = {}
+	end
+	maps[tostring(key)].id = {
+		["func"] = func,
+		["id"] = id,
+		["del"] = oneTime,
+	}
+end
+
+module.wipeFuncsInKey = function(key)
+	maps[key] = nil
+end
+
+module.wipeFuncByIdInKey = function(key, id)
+	maps[key][id] = nil
+end
+
+module.wipeAllFuncs = function()
+	maps = {}
+end
+
+module.isKeyDown = function(key)
+	return uis:IsKeyDown(key)
+end
+
+uis.InputBegan:Connect(function(inp, gpe)
+	if gpe == false then
+		local key = inp.KeyCode
+		--not in rbx menu
+		if maps[tostring(key)] ~= nil then
+			for i, v in pairs(maps[tostring(key)]) do
+				local func = v.func
+				coroutine.resume(coroutine.create(func))
+				if v.del == true then v = {} end
+			end
+		end
+	end
+end)
+
+return module
