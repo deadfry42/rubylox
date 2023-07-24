6c80e143c6492fe1f0f63d41591eff315f54ddfd
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-07-24 14:37:27 +0100

    incomplete

--- functionReferences/fadeinboy.lua
+++ functionReferences/fadeinboy.lua
@@ -1,3 +1,16 @@
 return function(loc)
-	print("fade brend")
+	local proftalk = require(loc.Parent.Parent.Scenes.PreGame.ProfTalk)
+	local assets = proftalk.retrieveAssets()
+	local tlp = assets[1]
+	local blp = assets[2]
+	local trp = assets[3]
+	local brp = assets[4]
+	local bir = assets[5]
+	local azur = assets[6]
+	tlp:Destroy()
+	blp:Destroy()
+	trp:Destroy()
+	brp:Destroy()
+	bir:Destroy()
+	azur:Destroy()
 end
